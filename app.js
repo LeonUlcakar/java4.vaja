748fa1a4036f6c782834829f2dea3c69a40c617c
LeonUlcakar <leon.ulcakar@gmail.com>
2022-12-02 11:01:33 +0100

    watwat

--- app.js
+++ app.js
@@ -52,4 +52,48 @@ function decrypt(word, x) {
 
 
 
+//console.log(decrypt(IMPORTANT_MESSAGE, 22));
+console.log(decrypt(encrypt(message, 3), 3));
+console.log(encrypt(message, 3));
 console.log(decrypt(IMPORTANT_MESSAGE, 22));
+    /*
+    
+    let CHARTABLE = ['a', 'b', 'c', '?', 'd',
+        'e', 'f', 'g', 'h', 'i',
+        'j', 'k', 'l', 'm', 'n',
+        'o', 'p', 'r', 's', 'š',
+        't', 'u', 'v', 'z', 'ž', '_', 0];
+    
+    let IMPORTANT_MESSAGE = [
+        'v', 'n', 'p', 'b', 'm', 'e',
+        'g', 'n', 'u', 'l', 'm', 'e',
+        'f', 'b', 'p', 'u', 'k', 'z',
+        'b', 'h', 'e', 'g', 'n', 'u',
+        'l', 'k', 'z', 'b', '?', 'j',
+        'e', 'h', 0];
+    
+    let code = "amfora";
+    let lenght = 0;
+    
+    for (let i in CHARTABLE) {
+        length = parseInt(i) + 1;
+    }
+    let _offset = 22;
+    
+    function encrypt(word, offset) {
+        tmp = "";
+        for (let i in word) {
+            tmp = tmp + crke[(crke.indexOf(word[i]) + offset >= length) ? crke.indexOf(word[i]) + offset - length : crke.indexOf(word[i]) + offset];
+        }
+        return tmp;
+    }
+    
+    function decrypt(word, offset) {
+        tmp = "";
+        for (let i in word) {
+            tmp = tmp + crke[(crke.indexOf(word[i]) - offset < 0) ? crke.indexOf(word[i]) - offset + length : crke.indexOf(word[i]) - offset];
+        }
+        return tmp;
+    }
+    decrypt(IMPORTANT_MESSAGE, 22);
+    */
